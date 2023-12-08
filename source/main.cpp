c1013e1fff4e1b74fa2d7a24a059b57d763413f7
Fabrizio Liani <f.liani@lancaster.ac.uk>
2023-12-08 11:05:03 +0000

    Compiles with memory init

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,17 @@
-#include "MicroBit.h"
-#include "samples/Tests.h"
-
-MicroBit uBit;
+#include "btstack_config.h"
+#include "bluetooth.h"
+#include "btstack_memory.h"
+#include "btstack_run_loop.h"
+#include "btstack_run_loop_embedded.h"
+#include "btstack_debug.h"
+#include "btstack_event.h"
+#include "hci.h"
+#include "hci_dump.h"
+#include "hci_transport.h"
+#include "hci_transport_h4.h"
+#include "l2cap.h"
 
 int main()
 {
-    uBit.init();
-
-    out_of_box_experience();
-
-    microbit_panic( 999 );
+    btstack_memory_init();
 }
-
